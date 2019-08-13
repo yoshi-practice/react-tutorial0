0b8800c76494d8222018f022db9e2893b22f81ee
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-08-14 08:47:28 +0900

    Update export/import component

--- src/js/client.js
+++ src/js/client.js
@@ -1,27 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-class Layout extends React.Component {
-
-  constructor(){
-      super();
-      this.name = "yoshi";
-  }
-
-  render() {
-
-    return (
-        <div>
-            <h1>number is {this.getNum(1)} !</h1>
-            <h2>My name is {this.name}</h2>
-        </div>
-    );
-  }
-
-  getNum(num){
-      return 1 + num;
-  }
-}
+ import React from "react";
+ import ReactDOM from "react-dom";
+ import Layout from "./components/Layout";
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
