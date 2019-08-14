a7437402867e0e80a3ef7033a8b248a2dd3276ff
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-08-14 10:19:59 +0900

    Update component再利用/配列

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Header from "./Header";
-import Foooter from "./Footer"
+import Footer from "./Footer";
 
 export default class Layout extends React.Component {
   render() {
-    return (
+    /* return (
       <div>
         <Header />
         <Header />
         <Header />
+        <Footer />
       </div>
+    ); */
+
+      // 配列を利用しての記述も可能。
+    let components = [<Header key='header'/> , <Footer key='footer'/>];
+    return (
+      <div>{components}</div>
     );
+
   }
 }
