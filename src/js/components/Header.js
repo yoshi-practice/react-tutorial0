6d7ff0190018bbe7ecde4aa8d17938a0e7724144
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-09-04 17:20:24 +0900

    feat: material ui and component

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Title from './Header/Title';
+import Buttons from './Header/Buttons';
 
 export default class Header extends React.Component{
     render() {
         return (
             <div>
                 <Title />
+                <Buttons />
             </div>
         )
     }
