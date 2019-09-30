e3d869a2948228315cc09199f391f6cdf4b3db69
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-09-30 13:54:32 +0900

    feat(coding): layout in scss

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import "./layout.scss";
 
 export default class Layout extends React.Component {
   render() {
